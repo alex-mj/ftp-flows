d4a8f9c377796cfefd6442e17d4d4a47b5c1c6ca
alex-mj-pradius <aleksandr.mozhayko@pradius.by>
2021-01-18 12:58:34 +0300

    +

--- ftp-flows.go
+++ ftp-flows.go
@@ -210,9 +210,11 @@ func (flow *flow) copyFileTo(dest *flowDir, fileSrc fileFromSource) bool {
 	}
 	if dest.LocalDir == "" {
 		destFtpName := dest.FtpSettings.FtpDir + fileSrc.OnlyFileName
+		fmt.Println(">>>>>>>")
 		fmt.Println(destFtpName)
 		fmt.Println(destFtpName)
 		fmt.Println(dest.FtpClient)
+
 		err = dest.FtpClient.Stor(destFtpName, file)
 		if err != nil {
 			Log.Error("#215" + err.Error())
